d80ab1425d10866e1d611311accf41c466c12cf2
Kevin DC <kevindelcastillo@ravn.co>
2023-03-01 13:40:32 -0500

    feat: Add main router

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { errorHandler } from './middlewares/error';
 import { customMorganMiddleware } from './middlewares/log';
+import router from './router';
 
 const server = express();
 
 server.use(customMorganMiddleware);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use('/api/v1', router);
 server.use(errorHandler);
 
 export default server;
