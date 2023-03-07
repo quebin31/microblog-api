d433ab0032bc171cd1e1c07bcb41d62988ac901a
Kevin DC <kevindelcastillo@ravn.co>
2023-03-07 12:27:02 -0500

    chore: Setup files for comments endpoints and operations

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import accountsRouter from './routes/accounts.router';
 import postsRouter from './routes/posts.router';
+import commentsRouter from './routes/comments.router';
 
 const router = Router();
 
 router.use('/accounts', accountsRouter);
 router.use('/posts', postsRouter);
+router.use('/comments', commentsRouter);
 
 export default router;
---
+++ src/routes/comments.router.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/');
+router.post('/');
+router.get('/:id');
+router.patch('/:id');
+router.delete('/:id');
+
+export default router;
