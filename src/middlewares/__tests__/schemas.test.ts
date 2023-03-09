424d7a87bab6019c5101e7abae306b86c4fa1232
Kevin DC <kevindelcastillo@ravn.co>
2023-03-09 10:44:59 -0500

    test: Add tests for schema validation middlewares

---
+++ src/middlewares/__tests__/schemas.test.ts
@@ -0,0 +1,71 @@
+import { buildExpressParams, buildReq } from '../../test/express';
+import { z } from 'zod';
+import { validateBody, validateQuery } from '../schemas';
+import { captor } from 'jest-mock-extended';
+import { BadRequestError } from '../../errors';
+
+describe('Validate request body', () => {
+  test('calls next with error if schema parsing fails', () => {
+    const originalBody = { c: 'hi' };
+    const req = buildReq({ body: originalBody });
+    const { res, next } = buildExpressParams();
+    const schema = z.object({ a: z.string() });
+    const validateMiddleware = validateBody(schema);
+
+    validateMiddleware(req, res, next);
+
+    const err = captor<any>();
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith(err);
+    expect(err.value).toBeInstanceOf(BadRequestError);
+
+    expect(req.body).toStrictEqual(originalBody);
+  });
+
+  test('updates request body on successful parse', () => {
+    const req = buildReq({ body: { a: 'hi', b: 'extra' } });
+    const { res, next } = buildExpressParams();
+    const schema = z.object({ a: z.string() });
+    const validateMiddleware = validateBody(schema);
+
+    validateMiddleware(req, res, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith();
+
+    expect(req.body).toStrictEqual({ a: 'hi' });
+  });
+});
+
+describe('Validate request query', () => {
+  test('calls next with error if schema parsing fails', () => {
+    const originalQuery = { a: 'desc' };
+    const req = buildReq({ query: originalQuery });
+    const { res, next } = buildExpressParams();
+    const schema = z.object({ q: z.string() });
+    const validateMiddleware = validateQuery(schema);
+
+    validateMiddleware(req, res, next);
+
+    const err = captor<any>();
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith(err);
+    expect(err.value).toBeInstanceOf(BadRequestError);
+
+    expect(req.query).toStrictEqual(originalQuery);
+  });
+
+  test('updates request query on successful parse', () => {
+    const req = buildReq({ query: { q: 'hi', b: 'extra' } });
+    const { res, next } = buildExpressParams();
+    const schema = z.object({ q: z.string() });
+    const validateMiddleware = validateQuery(schema);
+
+    validateMiddleware(req, res, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith();
+
+    expect(req.query).toStrictEqual({ q: 'hi' });
+  });
+});
