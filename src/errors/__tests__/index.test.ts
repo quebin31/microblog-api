90679f2a5ed1f0bae8cc708593095ffe54c7fa8b
Kevin DC <kevindelcastillo@ravn.co>
2023-03-09 09:18:35 -0500

    test: Add missing tests to errors

--- src/errors/__tests__/index.test.ts
+++ src/errors/__tests__/index.test.ts
@@ -1,4 +1,11 @@
-import { isRejectError, RejectError, Rejection } from '../index';
+import {
+  BadRequestError, ForbiddenError,
+  isRejectError,
+  NotFoundError,
+  RejectError,
+  Rejection, TooManyRequestsError,
+  UnauthorizedError,
+} from '../index';
 
 describe('Reject errors', () => {
   class DummyRejectError extends RejectError {
@@ -21,5 +28,55 @@ describe('Reject errors', () => {
 
     const closeToReject = { rejection: {}, reject: {} };
     expect(isRejectError(closeToReject)).toBeFalsy();
+  });
+
+  test('unauthorized error rejection', () => {
+    const message = 'missing token';
+    const err = new UnauthorizedError(message);
+    expect(err.rejection).toStrictEqual<Rejection>({
+      status: 401,
+      code: 'unauthorized',
+      message,
+    });
+  });
+
+  test('bad request error rejection', () => {
+    const message = 'bad request!';
+    const err = new BadRequestError(message);
+    expect(err.rejection).toStrictEqual<Rejection>({
+      status: 400,
+      code: 'bad_request',
+      message,
+    });
+  });
+
+  test('not found error rejection', () => {
+    const message = 'invalid user';
+    const err = new NotFoundError(message);
+    expect(err.rejection).toStrictEqual<Rejection>({
+      status: 404,
+      code: 'not_found',
+      message,
+    });
+  });
+
+  test('too many requests error rejection', () => {
+    const message = 'hold on';
+    const err = new TooManyRequestsError(message);
+    expect(err.rejection).toStrictEqual<Rejection>({
+      status: 429,
+      code: 'too_many_requests',
+      message,
+    });
+  });
+
+  test('forbidden error rejection', () => {
+    const message = 'nope';
+    const err = new ForbiddenError(message);
+    expect(err.rejection).toStrictEqual<Rejection>({
+      status: 403,
+      code: 'forbidden',
+      message,
+    });
   });
 });
