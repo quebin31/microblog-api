d032a802b4d20d506be23402573af37e9f240eac
Kevin DC <kevindelcastillo@ravn.co>
2023-03-15 13:01:20 -0500

    chore: Remove unneeded extra exports from repository mocks

--- src/repositories/__mocks__/comments.repository.ts
+++ src/repositories/__mocks__/comments.repository.ts
@@ -1,6 +1,5 @@
 import { commentsRepository as original } from '../comments.repository';
 import { mock } from 'jest-mock-extended';
 
-export type { GetAllOptions } from '../comments.repository';
 
 export const commentsRepository = mock<typeof original>();
--- src/repositories/__mocks__/posts.repository.ts
+++ src/repositories/__mocks__/posts.repository.ts
@@ -1,6 +1,4 @@
 import { postsRepository as original } from '../posts.repository';
 import { mock } from 'jest-mock-extended';
 
-export type { GetAllOptions } from '../posts.repository';
-
 export const postsRepository = mock<typeof original>();
