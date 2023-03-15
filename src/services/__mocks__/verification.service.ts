663d27619b914d005e5880d48fa7c6c8c67f8db2
Kevin DC <kevindelcastillo@ravn.co>
2023-03-15 11:58:48 -0500

    fix: Fix verification.service.ts mock

--- src/services/__mocks__/verification.service.ts
+++ src/services/__mocks__/verification.service.ts
@@ -1,6 +1,4 @@
-import {verificationService as original } from '../verification.service';
+import { verificationService as original } from '../verification.service';
 import { mock } from 'jest-mock-extended';
-
-export * from './verification.service';
 
 export const verificationService = mock<typeof original>();
